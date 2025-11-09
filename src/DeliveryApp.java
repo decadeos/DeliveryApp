a7bb92c08112a8695ffe9c2ffefb7aa2ae150fc6
eva <ohrimenkoed@gmail.com>
2025-11-09 15:04:00 +0300

    init project 5

--- src/DeliveryApp.java
+++ src/DeliveryApp.java
@@ -1,5 +1,5 @@
 public class DeliveryApp {
     public static void main(String[] args) {
-        System.out.println("init project 5");
+        System.out.println("init project 5"); // ahegik
     }
 }
